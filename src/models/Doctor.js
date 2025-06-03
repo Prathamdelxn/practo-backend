e38eab0706280e4fc9282ba3637cc37a736a8fa8
Prathamdelxn <godageprathmesh.delxn@gmail.com>
2025-06-03 18:10:31 +0530

    update -prathmesh

---
+++ src/models/Doctor.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  profileImage:{type:String,required:true},
+  dateOfBirth: { type: String, required: true },
+  gender: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone: { type: String, required: true },
+  specialty: { type: String, required: true },
+  supSpeciality: { type: String, },
+  experience: { type: Number, required: true },
+  qualifications: [String],
+  licenseNumber: { type: String, required: true, unique: true },
+  hospital: { type: String, required: true },
+  hospitalAddress: { type: String,  },
+  hospitalNumber:{ type: String, required: true },
+  isVerified: { type: Boolean, default: false },
+  role:{type:String,default:"doctor"},
+  available:{
+    days:[{ type: String,}],
+    time:{ type: String,},
+  }
+}, { timestamps: true });
+
+export default mongoose.models.Doctor || mongoose.model("Doctor", doctorSchema);
