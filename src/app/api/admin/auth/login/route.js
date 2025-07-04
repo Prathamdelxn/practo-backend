6090357b36d3ae2439d15e4a5d86ab6c23ca88cf
Prathamdelxn <godageprathmesh.delxn@gmail.com>
2025-07-04 11:18:19 +0530

    patientdata added -prathmesh

--- src/app/api/admin/auth/login/route.js
+++ src/app/api/admin/auth/login/route.js
@@ -36,7 +36,7 @@ export async function POST(req) {
   );
 
   const res = NextResponse.json({
-    message: "Login successful",
+    message: "Login successful", 
     token,
     user: {
       id: user._id,
